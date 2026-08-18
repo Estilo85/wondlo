0177e3bf329622d7c9a2bb54cb7e0c98836637a6
itumeleng-selonyane <itumeleng.selonyane@bothouniversity.com>
2026-08-18 20:22:48 +0200

    Remove Wrangler deploy command and src folder

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// This is a minimal worker to satisfy Wrangler
-export default {
-  async fetch(request, env, ctx) {
-    // This will be handled by Cloudflare Pages
-    return new Response('Hello from Wondlo Worker!', {
-      headers: { 'Content-Type': 'text/plain' }
-    });
-  }
-};
